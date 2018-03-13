e1c9e00418f50c348a77f7d53e48fe3972ffc167
jchin <justinchin.bcchs@gmail.com>
2018-03-13 14:31:33 -0700

    cleaned up files

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
-    PlayerData pd;
+    public PlayerData pd;
+
+    
     // Use this for initialization
     void Start () {
         pd = PlayerData.Instance;
