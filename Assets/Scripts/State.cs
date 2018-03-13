a6ad5c8bf9996a375a07f1dd7066c493dd28b61e
jchin <justinchin.bcchs@gmail.com>
2018-03-13 12:28:49 -0700

    First push of files

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,12 @@
+﻿public abstract class State {
+    protected Character character;
+
+    public abstract void Tick();
+
+    public virtual void OnStateEnter() { }
+    public virtual void OnStateExit() { }
+
+    public State(Character character) {
+        this.character = character;
+    }
+}
---
+++ Assets/TargetInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetInteraction : MonoBehaviour {
+    private BoxCollider boxCol;
+    public Renderer[] renderers;
+    public AudioClip audioClip;
+    public SequenceState seqState;
+    public AudioManagerSingleton audioManager;
+
+	// Use this for initialization
+	void Start () {
+        boxCol = GetComponent<BoxCollider>();
+        renderers = GetComponentsInChildren<Renderer>();
+
+        audioManager = AudioManagerSingleton.Instance;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Triggered");
+        foreach (Renderer renderer in renderers) {
+            renderer.material.color = Color.green;
+        }
+
+        MessageGameController();
+        PlayAudio();
+    }
+
+    public void PlayAudio() {
+
+        //audioManager.SetClip(seqState.successClip);
+        Debug.Log("playing audio");
+        audioManager.PlayClipAt(seqState.successClip, this.transform.position);
+
+    }
+
+    public void MessageGameController() {
+        Debug.Log("Tell GM trigger entered");
+    }
+
+
+}
